67f3c9a6afd589551a52afd5dfbfdc7003603467
Rebecca Buie <rebeccabuie@gmail.com>
2026-06-13 00:18:18 -0600

    Improve deck and study page layouts

--- app/controllers/study_cards_controller.rb
+++ app/controllers/study_cards_controller.rb
@@ -2,5 +2,8 @@ class StudyCardsController < ApplicationController
   def show
     @study_card = StudyCard.find(params[:id])
     @next_card = @study_card.next_card
+
+    @card_number = @study_card.position
+    @total_cards = @study_card.study_deck.study_cards.count
   end
 end
